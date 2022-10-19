4bca08519d1d03b4aa2f29d15cea52f18e715666
skin-is-black <oyekunleblessing2001@gmail.com>
2022-10-19 07:29:20 -0700

    C functions nest and loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,9 +16,9 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void times_table(void);
+void time_teble(void);
 int add(int, int);
 void print_to_98(int n);
-
+void print_time_table(int n);
 
 #endif

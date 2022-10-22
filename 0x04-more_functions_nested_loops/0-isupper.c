7ae2c07c93593faad0842e499e17b5afd871ebe4
Awolowo <103498356+skin-is-black@users.noreply.github.com>
2022-10-22 02:29:27 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _isupper - Check if paramter c represents an uppercase letter.
+ * _isupper - Check if a character uppercase 
  * @c: Int representing a character
  * Return: Always 0.
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
-	return (0);
+	else 
+	{
+	       return (0);
+        }
 }
